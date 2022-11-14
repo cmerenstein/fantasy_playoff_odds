6de9a0f1dd83972cddf6a4c028e5eeff42aef330
Carter <cmerenstein@gmail.com>
2022-11-13 21:13:24 -0500

    fix for flask

--- flask_server.py
+++ flask_server.py
@@ -1,23 +1,61 @@
 from flask import Flask, jsonify, render_template, request, make_response
 import playoff_odds
+import pickle
 
 app = Flask(__name__)
+
+def build_game_cell(game, week_num, game_num):
+    name1 = playoff_odds.teams[int(game[0])-1]
+    name2 = playoff_odds.teams[int(game[1])-1]
+
+    id1 = f"{week_num}-{game_num}-1"
+    id2 = f"{week_num}-{game_num}-2"
+    cell = f'<div><input type="radio" id="{id1}" data-team="{game[0]}" data-selected="false" onclick=selected(this)>'
+    cell += f'<label for="{id1}">{name1}</label>'
+    #cell += "<br />vs. <br />"
+    cell += "<br />"
+    cell += f'<div><input type="radio" id="{id2}" data-team="{game[1]}" data-selected="false" onclick=selected(this)>'
+    cell += f'<label for="{id2}">{name2}</label>'
+    return cell
+
+def build_schedule_table(schedule):
+    t = "<table>"
+
+    for i, week in enumerate(schedule):
+        t += f"<tr><th>Week {i+1}</th>"
+        for j, game in enumerate(week):
+            t += f"<td>{build_game_cell(game, i, j)}</td>"
+        t += "</tr>"
+
+    t += "</table>"
+    return t
 
 @app.route('/')
 def home(methods = ["POST", "GET"]):
   if request.method == "POST":
    data = request.get_json() 
   else:
-    return render_template("index.html") 
+    fl2 = open("data_2022/schedule.pkl", 'rb') 
+    schedule = pickle.load(fl2) 
+    print(schedule)
+    print(len(schedule))
+    return render_template("index.html",
+            schedule_table = build_schedule_table(schedule))
 
 
 @app.route('/post', methods=["POST"])
 def get_click():
+  picks = {}
+
   req = request.get_json()
-  print(req)
+  if "picks" in req:
+    for k, v in req["picks"].items():
+      picks[int(k)] = v
 
   ## run simulation
-  odds = playoff_odds.simulate()
+  odds = playoff_odds.simulate(picks=picks)
+
+  print(odds)
 
   res = make_response(odds, 200)
 
--- playoff_odds.py
+++ playoff_odds.py
@@ -4,7 +4,11 @@ import random
 import pickle 
 import json
 import math
+import datetime
 
+global N_TEAMS
+N_TEAMS = 10
+teams = ["Caleb", "Simon", "Levi", "Traci", "Dan", "Zach", "Alex", "Carter", "Anna", "Jenni"]
 
 def random_winner(matchup):
   team_0 = matchup[0]
@@ -32,7 +36,26 @@ def picked_winner(random_winner, matchup, week,  picks):
     return random_winner
 
 
-def simulate_seasons(schedule, actual_record, n_iter, picks):
+def get_last_game(c_wins):
+  '''
+    Figure out which game was the last complete one, using the # of current wins and day of week
+  '''
+  last_week_wins = [0] * N_TEAMS
+  last_game = 0
+  for week_index, wins in enumerate(c_wins):
+    day_of_week = datetime.datetime.today().weekday()
+    ## If the day is Tuesday or Wednesday, we can use this week, otherwise, use last week
+    if day_of_week > 0 & day_of_week < 3:
+      if wins != last_week_wins:
+        last_game = week_index
+    else:
+      if wins != last_week_wins:
+        last_game = week_index - 1
+    last_week_wins = wins
+  return(last_game)
+
+
+def simulate_seasons(last_game, schedule, actual_record, n_iter, picks):
   ''' 
       Simulate the remaining games in a season and return an array of final wins.
   '''
@@ -41,20 +64,20 @@ def simulate_seasons(schedule, actual_record, n_iter, picks):
   #
     c_wins = actual_record.copy()
   #
-    for week in range(9, 15):
-      ## get matchups this week
-      matchups = schedule[week]
+    ## numbers get weird here, just print matchups to figure it out
+    for sim_week in range(last_game, 15): ## start the week *after* the last week
+      matchups = schedule[sim_week]
     #
     # setup empty wins and points this week
-      wins = c_wins[week - 1].copy()
+      wins = c_wins[sim_week - 1].copy()
     #
       for matchup in matchups:
         winner = random_winner(matchup) ## get coin-toss winner
     #
-        winner = picked_winner(winner, matchup, week, picks) ## override if there's a picked winner
+        winner = picked_winner(winner, matchup, sim_week, picks) ## override if there's a picked winner
         wins[int(winner) - 1] += 1
     #
-      c_wins[week] = wins
+      c_wins[sim_week] = wins
   #
     final_wins.append(c_wins[-1])
   return final_wins
@@ -66,7 +89,7 @@ def get_playoffs(wins):
     First we find the cutoff (# of wins of the 4th place team), then everyone above makes it.
     To break ties, we randomly pick from teams at the cutoff.
   '''
-  cutoff = sorted(wins)[4]
+  cutoff = sorted(wins)[-4]
   made_playoffs = np.where(np.array(wins) > cutoff)[0].tolist()
 #
 # now randomly chose a team at the cutoff
@@ -80,7 +103,7 @@ def playoff_odds(final_wins):
   '''
     Turn all the simulated wins into one set of playoff odds
   '''
-  playoffs = [0] * 8
+  playoffs = [0] * N_TEAMS
   for wins in final_wins:
     teams = get_playoffs(wins)
     for team in teams:
@@ -91,7 +114,7 @@ def playoff_odds(final_wins):
 
 
 ## ------------- Run Simulations ----------------------
-def simulate(record_file = "data/actual_record.pkl", schedule_file = "data/schedule.pkl",
+def simulate(record_file = "data_2022/actual_record.pkl", schedule_file = "data_2022/schedule.pkl",
             n_iter = 10000, picks = {}):
   '''
     Main function to run the whole simulation
@@ -104,13 +127,14 @@ def simulate(record_file = "data/actual_record.pkl", schedule_file = "data/sched
   schedule = pickle.load(fl2)
 #
   ## Simulate 10,000 seasons with this schedule
-  final_wins = simulate_seasons(schedule, actual_record, 10000, picks)
+  last_game = get_last_game(actual_record)
+  final_wins = simulate_seasons(last_game, schedule, actual_record, 10000, picks)
 #
   ## Get final playoff odds from these simulations
   odds = playoff_odds(final_wins)
 #
   ## Display using actual team names
-  teams = ["Simon", "Levi", "Traci", "Caleb", "Dan", "Zach", "Alex", "Carter"] 
+  teams = ["Caleb", "Simon", "Levi", "Traci", "Dan", "Zach", "Alex", "Carter", "Anna", "Jenni"]
   print(pd.DataFrame( [ teams, odds]))
 #
   ## Make json object for the results
