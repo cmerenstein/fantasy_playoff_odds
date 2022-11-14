ab539c5530e5ad66b9b9c5f95483982ec739f9df
Carter <cmerenstein@gmail.com>
2022-11-13 21:31:38 -0500

    fix radial buttons again

--- static/js/handler.js
+++ static/js/handler.js
@@ -2,14 +2,18 @@
 var b1 = document.getElementById("b1")
 var buttons = document.getElementsByClassName("button")
 
+document.onload = reset_table
+
 console.log(buttons)
 
-b1.onclick = function()  {
+b1.onclick = read_table
+
+function submit(picks)  {
 
   fetch('/post', {
     method: "POST",
     credentials: "include",
-    body: JSON.stringify({"test":"other"}),
+    body: JSON.stringify({"picks":picks}),
     cache: "no-cache",
     headers: new Headers({
       "content-type": "application/json"
@@ -42,4 +46,61 @@ b1.onclick = function()  {
 
 }
 
+function selected(t) {
+	console.log(t);
+	console.log(t.dataset.selected);
+	if (t.dataset.selected == "false") {
+		t.dataset.selected = "true";
+		console.log("uh");
+	} else {
+		t.dataset.selected = "false";
+		t.checked = false;
+	}
 
+	// Make sure only one is selected
+	if (t.checked) {
+		base_id = t.id.substr(0, t.id.length-1);
+		team_num = t.id.substr(-1);
+		if (team_num == "1") {
+			document.getElementById(base_id+"2").checked = false;
+		} else {
+			document.getElementById(base_id+"1").checked = false;
+		}
+	}
+
+	read_table();
+}
+
+function read_table() {
+  res = {};
+  for (let week = 0; week < 15; week++) {
+    week_res = [];
+    for (let game = 0; game < 4; game++) {
+      team1 = document.getElementById(week+"-"+game+"-1");
+      team2 = document.getElementById(week+"-"+game+"-2");
+      if (team1.checked) {
+        week_res.push(team1.dataset.team)
+
+        if (team2.checked) {
+          console.log("!!! both teams selected");
+        }
+      } else if (team2.checked) {
+        week_res.push(team2.dataset.team)
+      }
+    }
+    res[week] = week_res;
+  }
+  console.log(res);
+
+  submit(res);
+}
+
+function reset_table() {
+  for (let week = 0; week < 15; week++) {
+    week_res = [];
+    for (let game = 0; game < 4; game++) {
+      document.getElementById(week+"-"+game+"-1").checked = false;
+      document.getElementById(week+"-"+game+"-2").checked = false;
+    }
+  }
+}
